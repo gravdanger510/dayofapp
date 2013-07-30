82adba389b60dcfb8cb2db9b7ccc2e774d7f4900
Tyler Mitchell <tylerlmitchell@yahoo.com>
2013-07-30 19:34:10 -0400

    Created dayof and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dayof::Application.config.secret_token = '697d4a6aff133b27b86710166e6e53dde940ea5f7d535bfa9d3aa2578b45861286a4141e8bf8fa4bfe2b0ae4e8c5a8cef330be10ad020bba542fde5618bf2f00'
